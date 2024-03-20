6fc922ff12040daf799a024cb3cb8c3f66888c74
Vadim <vboldireff01@gmail.com>
2024-03-20 19:00:24 -0400

    Add PrioKey for selectables

--- Source/Engine/UI/include/SurvivantUI/Interfaces/ISelectable.h
+++ Source/Engine/UI/include/SurvivantUI/Interfaces/ISelectable.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SurvivantUI/MenuItems/PopupMenu.h>
+#include "SurvivantCore/Utility/Utility.h"
 
 #include <string>
 
@@ -10,6 +11,30 @@ namespace SvUI::Interfaces
 	{
 	public:
 		virtual ~ISelectable() = default;
+
+		struct SelectablePrioKey
+		{
+			std::string m_name = "";
+			size_t m_priority = 0;
+
+
+			bool operator <(const SelectablePrioKey& p_rhs) const
+			{
+				if (this->m_priority != p_rhs.m_priority)
+					return this->m_priority > p_rhs.m_priority;
+
+				int val = SvCore::Utility::CompareAlphabeticly(this->m_name, p_rhs.m_name);
+
+				if (val == -1)
+					return true;
+
+				if (val == 1)
+					return false;
+
+				//if equal check case sensitivity
+				return this->m_name < p_rhs.m_name;
+			}
+		};
 
 		virtual const std::string&	GetIcon() = 0;
 		virtual const std::string&	GetName() = 0;
