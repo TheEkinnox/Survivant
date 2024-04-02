4617e21b9b7c0db578fdfe15ff81aa4cce34c047
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-02 16:30:48 -0400

    Implement light component serialization

--- Source/Engine/Rendering/include/SurvivantRendering/Enums/ELightType.h
+++ Source/Engine/Rendering/include/SurvivantRendering/Enums/ELightType.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <SurvivantCore/Debug/Assertion.h>
+
 #include <cstdint>
 
 namespace SvRendering::Enums
@@ -13,4 +15,52 @@ namespace SvRendering::Enums
         POINT,
         SPOT
     };
+
+    /**
+     * \brief Gets the given light type's string representation
+     * \param p_type The light type to stringify
+     * \return The given type's string representation
+     */
+    inline std::string LightTypeToString(const ELightType p_type)
+    {
+        switch (p_type)
+        {
+        case ELightType::AMBIENT:
+            return "ambient";
+        case ELightType::DIRECTIONAL:
+            return "directional";
+        case ELightType::POINT:
+            return "point";
+        case ELightType::SPOT:
+            return "spot";
+        default:
+            ASSERT(false, "Unsupported light type");
+            return {};
+        }
+    }
+
+    /**
+     * \brief Gets the light type represented by the given string
+     * \param p_type The type string to convert
+     * \return The type corresponding to the given string
+     */
+    inline ELightType StringToLightType(std::string p_type)
+    {
+        SvCore::Utility::ToLowerInPlace(p_type);
+
+        if (p_type == "ambient")
+            return ELightType::AMBIENT;
+
+        if (p_type == "directional")
+            return ELightType::DIRECTIONAL;
+
+        if (p_type == "point")
+            return ELightType::POINT;
+
+        if (p_type == "spot")
+            return ELightType::SPOT;
+
+        ASSERT(false, "Unsupported light type");
+        return ELightType::AMBIENT;
+    }
 }
