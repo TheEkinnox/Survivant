8a8d822eca1665c84bdacc74ab701b8c79029da4
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-05 09:40:53 -0400

    Implement model component serialization

--- Source/Engine/Rendering/include/SurvivantRendering/Components/ModelComponent.h
+++ Source/Engine/Rendering/include/SurvivantRendering/Components/ModelComponent.h
@@ -2,6 +2,8 @@
 #include "SurvivantRendering/Resources/Material.h"
 #include "SurvivantRendering/Resources/Model.h"
 
+#include <SurvivantCore/ECS/ComponentRegistry.h>
+
 namespace SvRendering::Components
 {
     struct ModelComponent
@@ -9,4 +11,15 @@ namespace SvRendering::Components
         SvCore::Resources::ResourceRef<Resources::Model>    m_model;
         SvCore::Resources::ResourceRef<Resources::Material> m_material;
     };
+}
+
+namespace SvCore::ECS
+{
+    template <>
+    bool ComponentRegistry::ToJson<SvRendering::Components::ModelComponent>(
+        const SvRendering::Components::ModelComponent&, rapidjson::Writer<rapidjson::StringBuffer>&, const EntitiesMap&);
+
+    template <>
+    bool ComponentRegistry::FromJson<SvRendering::Components::ModelComponent>(
+        SvRendering::Components::ModelComponent&, const rapidjson::Value&);
 }
