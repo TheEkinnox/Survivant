374eb4333543c659959f1b870f150657901276f0
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-10 10:16:28 -0400

    Fix resource updates requiring manual rebuild

--- CMake/CopyResources.cmake
+++ CMake/CopyResources.cmake
@@ -1,18 +1,17 @@
 function(copy_resources TARGET_NAME)
+	set(CMAKE_FOLDER .utils)
+
 	# remove the existing assets folder from the build directory (ensures removes are also replicated)
-	add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E rm -rf $<TARGET_FILE_DIR:${TARGET_NAME}>/assets
-	)
-
 	# copy the assets folder in the target build directory
-	add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E copy_directory ${SURVIVANT_ASSETS_DIR} $<TARGET_FILE_DIR:${TARGET_NAME}>/assets
-	)
-
 	# remove unwanted files
-	add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+	add_custom_target(${TARGET_NAME}_Resources
+		DEPENDS ${RESOURCES_STAMP}
+		COMMAND ${CMAKE_COMMAND} -E rm -rf $<TARGET_FILE_DIR:${TARGET_NAME}>/assets
+		COMMAND ${CMAKE_COMMAND} -E copy_directory ${SURVIVANT_ASSETS_DIR} $<TARGET_FILE_DIR:${TARGET_NAME}>/assets
 		COMMAND ${CMAKE_COMMAND} -E rm -f $<TARGET_FILE_DIR:${TARGET_NAME}>/assets/.gitkeep $<TARGET_FILE_DIR:${TARGET_NAME}>/assets/CMakeLists.txt
 	)
-	
+
+	add_dependencies(${TARGET_NAME} ${TARGET_NAME}_Resources)
+
 	message(STATUS "Resources added to target ${TARGET_NAME}")
 endfunction()
