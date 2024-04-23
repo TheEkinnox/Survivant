8deaf54d29e17eed03ea69d7d8db50ddb19d6a85
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-23 19:41:27 -0400

    Fix TempDefaultScene.h compile issues

--- Source/Editor/include/SurvivantEditor/App/TempDefaultScene.h
+++ Source/Editor/include/SurvivantEditor/App/TempDefaultScene.h
@@ -1,14 +1,19 @@
 //TempDefaultScene.h
 #pragma once
 
+#include "SurvivantEditor/App/EngineApp.h"
+
+#include <SurvivantApp/Inputs/InputManager.h>
+#include <SurvivantApp/Inputs/KeyboardInputs.h>
+#include <SurvivantApp/Inputs/MouseInputs.h>
+
 #include <SurvivantCore/Debug/Assertion.h>
 #include <SurvivantCore/ECS/EntityHandle.h>
 #include <SurvivantCore/ECS/Scene.h>
 #include <SurvivantCore/ECS/SceneView.h>
 #include <SurvivantCore/ECS/Components/Hierarchy.h>
-#include <SurvivantCore/Utility/FileSystem.h>
-#include <SurvivantCore/Utility/Timer.h>
 
+#include <SurvivantRendering/Components/CameraComponent.h>
 #include <SurvivantRendering/Components/LightComponent.h>
 #include <SurvivantRendering/Components/ModelComponent.h>
 #include <SurvivantRendering/Core/Camera.h>
@@ -20,15 +25,9 @@
 #include <SurvivantRendering/RHI/IUniformBuffer.h>
 #include <SurvivantRendering/RHI/OpenGL/OpenGLTexture.h>
 
-#include "SurvivantApp/Inputs/InputManager.h"
-#include "SurvivantApp/Inputs/KeyboardInputs.h"
-#include "SurvivantApp/Inputs/MouseInputs.h"
-#include "SurvivantApp/Windows/Window.h"
-
-#include "SurvivantEditor/App/EngineApp.h"
-
 using namespace LibMath;
 using namespace SvCore::ECS;
+using namespace SvCore::Resources;
 using namespace SvCore::Utility;
 using namespace SvRendering::Core;
 using namespace SvRendering::Components;
@@ -37,14 +36,13 @@ using namespace SvRendering::Geometry;
 using namespace SvRendering::Resources;
 using namespace SvRendering::RHI;
 
-
 struct Rotator
 {
     Quaternion m_speed;
 };
 
 template <>
-inline void ComponentTraits::OnAdd<Rotator>(EntityHandle& p_handle, Rotator&)
+inline void ComponentTraits::OnAdd(EntityHandle& p_handle, Rotator&)
 {
     if (!p_handle.Has<Transform>())
         p_handle.Make<Transform>();
@@ -63,11 +61,11 @@ namespace ToRemove
     //SceneView<Temporary>                  temporariesView(scene);
     //SceneView<const UserInput, Transform> userInputsView(scene);
     //SceneView<const Rotator, Transform>   rotatorsView(scene);
-    
+
     class GameInfo
     {
     public:
-        static inline Vector2 moveInput, rotateInput = Vector2::zero();
+        static inline Vector2 moveInput = Vector2::zero(), rotateInput = Vector2::zero();
         static inline EntityHandle gameCamera = EntityHandle();
     };
 
@@ -84,16 +82,16 @@ namespace ToRemove
         float m_lifeSpanSec;
     };
 
-    inline std::shared_ptr<ITexture> GetTexture()
+    inline ResourceRef<ITexture> GetTexture()
     {
-        static std::shared_ptr<ITexture> texture = ITexture::Create();
-        static bool                      isLoaded = false;
+        static ResourceRef<ITexture> texture("assets/textures/grid.png");
+
+        static bool isLoaded = false;
 
         if (isLoaded)
             return texture;
 
-        ASSERT(texture->Load("assets/textures/grid.png"));
-        ASSERT(texture->Init());
+        ASSERT(texture);
 
         texture->SetFilters(ETextureFilter::NEAREST, ETextureFilter::NEAREST);
         texture->SetWrapModes(ETextureWrapMode::REPEAT, ETextureWrapMode::REPEAT);
@@ -257,17 +255,17 @@ namespace ToRemove
             CHECK(i == 4, "Setting a component for an existing entity should have updated it's value");
         }
 
-        CHECK(scene.GetStorage<Entity>().GetCount() == 5);
-        CHECK(scene.GetStorage<int>().GetCount() == 5);
+        CHECK(scene.GetStorage<Entity>().size() == 5);
+        CHECK(scene.GetStorage<int>().size() == 5);
 
         scene.Remove<const int>(Entity(1));
-        CHECK(scene.GetStorage<int>().GetCount() == 4);
+        CHECK(scene.GetStorage<int>().size() == 4);
 
         scene.Remove<int>(*scene.Get<int>(lastEntity));
-        CHECK(scene.GetStorage<int>().GetCount() == 3);
+        CHECK(scene.GetStorage<int>().size() == 3);
 
         scene.Remove<int>(lastEntity);
-        CHECK(scene.GetStorage<int>().GetCount() == 3);
+        CHECK(scene.GetStorage<int>().size() == 3);
 
         SceneView<int, char, float> view(scene);
 
@@ -275,9 +273,9 @@ namespace ToRemove
         lastEntity.Set<char>('j');
         lastEntity.Set<float>(2.5f);
 
-        CHECK(scene.GetStorage<int>().GetCount() == 4);
-        CHECK(scene.GetStorage<float>().GetCount() == 1);
-        CHECK(scene.GetStorage<char>().GetCount() == 1);
+        CHECK(scene.GetStorage<int>().size() == 4);
+        CHECK(scene.GetStorage<float>().size() == 1);
+        CHECK(scene.GetStorage<char>().size() == 1);
 
         const char c1 = *view.Get<char>(lastEntity);
         auto [i1, f1] = view.Get<int, float>(lastEntity);
@@ -315,10 +313,10 @@ namespace ToRemove
 
         lastEntity.Destroy();
 
-        CHECK(scene.GetStorage<Entity>().GetCount() == 4);
-        CHECK(scene.GetStorage<int>().GetCount() == 3);
-        CHECK(scene.GetStorage<float>().GetCount() == 0);
-        CHECK(scene.GetStorage<char>().GetCount() == 0);
+        CHECK(scene.GetStorage<Entity>().size() == 4);
+        CHECK(scene.GetStorage<int>().size() == 3);
+        CHECK(scene.GetStorage<float>().size() == 0);
+        CHECK(scene.GetStorage<char>().size() == 0);
 
         auto [iDestroyed, cDestroyed, fDestroyed] = view.Get(lastEntity);
 
@@ -331,10 +329,10 @@ namespace ToRemove
 
         lastEntity = scene.Create();
 
-        CHECK(scene.GetStorage<Entity>().GetCount() == 5);
-        CHECK(scene.GetStorage<int>().GetCount() == 3);
-        CHECK(scene.GetStorage<float>().GetCount() == 0);
-        CHECK(scene.GetStorage<char>().GetCount() == 0);
+        CHECK(scene.GetStorage<Entity>().size() == 5);
+        CHECK(scene.GetStorage<int>().size() == 3);
+        CHECK(scene.GetStorage<float>().size() == 0);
+        CHECK(scene.GetStorage<char>().size() == 0);
 
         for ([[maybe_unused]] const auto _ : view)
             CHECK(false, "The only releveant entity should have been destroyed");
@@ -346,48 +344,44 @@ namespace ToRemove
         EntityHandle camEntity = p_scene.Create();
         GameInfo::gameCamera = camEntity;
 
-        Camera& cam = camEntity.Make<Camera>(perspectiveProjection(90_deg, 4.f / 3.f, .01f, 14.f));
-        cam.SetClearColor(Color::gray);
+        CameraComponent& cam = camEntity.Make<CameraComponent>();
+        cam.SetClearColor(Color::gray).SetPerspective(90_deg, .01f, 14.f).SetAspect(4.f / 3.f);
 
         const Vector3 camPos(0.f, 1.8f, 2.f);
         camEntity.Make<Transform>(camPos, Quaternion::identity(), Vector3::one());
 
         camEntity.Make<UserInput>(CAM_MOVE_SPEED, CAM_ROTATION_SPEED);
 
-        std::shared_ptr<Model> cube = std::make_shared<Model>();
+        ResourceRef<Model> cube("assets/models/cube.obj");
+        ASSERT(cube);
 
-        ASSERT(cube->Load("assets/models/cube.obj"), "Failed to load model");
-        ASSERT(cube->Init(), "Failed to initialize model");
+        ResourceRef<IShader> unlitShader(UNLIT_SHADER_PATH);
+        ASSERT(unlitShader);
 
-        std::shared_ptr<IShader> unlitShader = IShader::Create();
-        ASSERT(unlitShader->Load(UNLIT_SHADER_PATH), "Failed to load shader at path \"%s\"", UNLIT_SHADER_PATH);
-        ASSERT(unlitShader->Init(), "Failed to initialize shader at path \"%s\"", UNLIT_SHADER_PATH);
+        ResourceRef<IShader> litShader(LIT_SHADER_PATH);
+        ASSERT(litShader);
 
-        std::shared_ptr<IShader> litShader = IShader::Create();
-        ASSERT(litShader->Load(LIT_SHADER_PATH), "Failed to load shader at path \"%s\"", LIT_SHADER_PATH);
-        ASSERT(litShader->Init(), "Failed to initialize shader at path \"%s\"", LIT_SHADER_PATH);
-
-        std::shared_ptr<Material> whiteMaterial = std::make_shared<Material>(unlitShader);
-        whiteMaterial->GetProperty<std::shared_ptr<ITexture>>("u_diffuse") = GetTexture();
+        ResourceRef whiteMaterial("internal:whiteMaterial", new Material(unlitShader));
+        whiteMaterial->GetProperty<ResourceRef<ITexture>>("u_diffuse") = GetTexture();
         whiteMaterial->GetProperty<Vector4>("u_tint") = Color::white;
 
-        std::shared_ptr<Material> redMaterial = std::make_shared<Material>(*whiteMaterial);
+        ResourceRef redMaterial("internal:redMaterial", new Material(*whiteMaterial));
         redMaterial->GetProperty<Vector4>("u_tint") = Color::red;
 
-        std::shared_ptr<Material> greenMaterial = std::make_shared<Material>(*whiteMaterial);
+        ResourceRef greenMaterial("internal:greenMaterial", new Material(*whiteMaterial));
         greenMaterial->GetProperty<Vector4>("u_tint") = Color::green;
 
-        std::shared_ptr<Material> blueMaterial = std::make_shared<Material>(*whiteMaterial);
+        ResourceRef blueMaterial("internal:blueMaterial", new Material(*whiteMaterial));
         blueMaterial->GetProperty<Vector4>("u_tint") = Color::blue;
 
-        std::shared_ptr<Material> yellowMaterial = std::make_shared<Material>(*whiteMaterial);
+        ResourceRef yellowMaterial("internal:yellowMaterial", new Material(*whiteMaterial));
         yellowMaterial->GetProperty<Vector4>("u_tint") = Color::yellow;
 
-        std::shared_ptr<Material> magentaMaterial = std::make_shared<Material>(*whiteMaterial);
+        ResourceRef magentaMaterial("internal:magentaMaterial", new Material(*whiteMaterial));
         magentaMaterial->GetProperty<Vector4>("u_tint") = Color::magenta;
 
-        std::shared_ptr<Material> litMaterial = std::make_shared<Material>(litShader);
-        litMaterial->GetProperty<std::shared_ptr<ITexture>>("u_diffuse") = GetTexture();
+        ResourceRef litMaterial("internal:litMaterial", new Material(litShader));
+        litMaterial->GetProperty<ResourceRef<ITexture>>("u_diffuse") = GetTexture();
         litMaterial->GetProperty<Vector4>("u_tint") = Color::white;
         litMaterial->GetProperty<Vector4>("u_specularColor") = Color(.2f, .2f, .2f);
         litMaterial->GetProperty<float>("u_shininess") = 32.f;
@@ -469,25 +463,26 @@ namespace ToRemove
 
     void inline DrawScene(Scene& p_scene)
     {
-        SceneView<Camera>                                cameras(p_scene);
+        SceneView<CameraComponent>                       cameras(p_scene);
         SceneView<const ModelComponent, const Transform> renderables(p_scene);
 
         for (const auto camEntity : cameras)
         {
-            Camera& cam = *cameras.Get<Camera>(camEntity);
+            CameraComponent& cam = *cameras.Get<CameraComponent>(camEntity);
 
             if (const Transform* transform = p_scene.Get<const Transform>(camEntity))
             {
-                cam.SetView(transform->getWorldMatrix().inverse());
-                BindCamUBO(cam.GetViewProjection(), transform->getWorldPosition());
+                cam.Recalculate(transform->getWorldMatrix().inverse());
+                BindCamUBO(cam->GetViewProjection(), transform->getWorldPosition());
             }
             else
             {
-                BindCamUBO(cam.GetViewProjection(), Vector3::zero());
+                cam.Recalculate(Matrix4(1.f));
+                BindCamUBO(cam->GetViewProjection(), Vector3::zero());
             }
 
             cam.Clear();
-            const Frustum camFrustum = cam.GetFrustum();
+            const Frustum camFrustum = cam->GetFrustum();
 
             for (const auto modelEntity : renderables)
             {
@@ -552,11 +547,6 @@ namespace ToRemove
     using namespace LibMath;
     using namespace SvRendering::RHI;
     using namespace SvRendering::Enums;
-
-
-    static inline Material whiteMaterial;
-    static inline Material redMaterial;
-    static inline Material litMaterial;
 
     std::shared_ptr<Scene> inline TestCreateDefaultScene()
     {
