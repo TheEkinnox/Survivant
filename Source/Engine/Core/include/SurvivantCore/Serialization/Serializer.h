4dc25f3f7427b3f884cc18bbe8a085d2dcf16515
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-28 21:24:24 -0400

    Add json file loading function

--- Source/Engine/Core/include/SurvivantCore/Serialization/Serializer.h
+++ Source/Engine/Core/include/SurvivantCore/Serialization/Serializer.h
@@ -1,11 +1,24 @@
 #pragma once
+#include <string>
+
 #include <rapidjson/document.h>
 #include <rapidjson/writer.h>
+#include <rapidjson/ostreamwrapper.h>
 
 namespace SvCore::Serialization
 {
     using JsonWriter = rapidjson::Writer<rapidjson::StringBuffer>;
+    using JsonOStream = rapidjson::OStreamWrapper;
+    using JsonFileWriter = rapidjson::Writer<JsonOStream>;
+    using JsonDocument = rapidjson::Document;
     using JsonValue = rapidjson::Value;
+
+    /**
+     * \brief Loads the json document at the given path
+     * \param p_path The json file's path
+     * \return The loaded json document
+     */
+    JsonDocument LoadJsonFile(const std::string& p_path);
 
     template <typename T>
     bool ToJson(const T& value, JsonWriter& writer) requires (!std::is_enum_v<T>);
