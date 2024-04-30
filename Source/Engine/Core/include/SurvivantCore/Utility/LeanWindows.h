319c87cad3a0ddfa8bba3b04a8cd8ea9661f4601
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-30 11:53:47 -0400

    Add #ifndef checks for LeanWin defines

--- Source/Engine/Core/include/SurvivantCore/Utility/LeanWindows.h
+++ Source/Engine/Core/include/SurvivantCore/Utility/LeanWindows.h
@@ -1,5 +1,16 @@
 #pragma once
+
+#ifdef _WIN32
+
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN	// Disables unnecessary windows features
+#endif
+
+#ifndef NOMINMAX
 #define NOMINMAX			// Avoids conflicts with LibMath's min and max
+#endif
+
 #undef APIENTRY				// Avoids conflicts with some other libs (e.g: GLFW)
 #include <windows.h>
+
+#endif // _WIN32
