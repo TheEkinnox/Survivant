b0ead32e3d58ad30375569d8dc8159de964d6f24
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-05-13 21:40:34 -0400

    Add layer mask to model component

--- Source/Engine/Rendering/include/SurvivantRendering/Components/ModelComponent.h
+++ Source/Engine/Rendering/include/SurvivantRendering/Components/ModelComponent.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "SurvivantRendering/Core/Layer.h"
 #include "SurvivantRendering/Resources/Material.h"
 #include "SurvivantRendering/Resources/Model.h"
 
@@ -10,6 +11,7 @@ namespace SvRendering::Components
     {
         SvCore::Resources::ResourceRef<Resources::Model>    m_model;
         SvCore::Resources::ResourceRef<Resources::Material> m_material;
+        Core::LayerMask                                     m_layerMask;
     };
 }
 
@@ -17,8 +19,8 @@ namespace SvCore::ECS
 {
     template <>
     bool ComponentRegistry::ToJson(
-        const SvRendering::Components::ModelComponent&, SvCore::Serialization::JsonWriter&, const EntitiesMap&);
+        const SvRendering::Components::ModelComponent&, Serialization::JsonWriter&, const EntitiesMap&);
 
     template <>
-    bool ComponentRegistry::FromJson(SvRendering::Components::ModelComponent&, const SvCore::Serialization::JsonValue&, Scene*);
+    bool ComponentRegistry::FromJson(SvRendering::Components::ModelComponent&, const Serialization::JsonValue&, Scene*);
 }
