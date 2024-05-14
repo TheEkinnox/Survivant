7fba57a119119813e70c583da2bbd899491bc4f9
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-05-13 22:35:14 -0400

    Add physics functions to lua script template

--- Resources/scripts/template.lua
+++ Resources/scripts/template.lua
@@ -1,22 +1,42 @@
 local %%script = {}
 
--- onInit is called when the script is added
+-- OnInit is called when the script is added
 function %%script:OnInit()
 end
 
--- onStart is called when the application starts
+-- OnStart is called when the application starts
 function %%script:OnStart()
 end
 
--- onUpdate is called once per frame
+-- OnUpdate is called once per frame
 function %%script:OnUpdate(deltaTime)
 end
 
--- onStop is called when the application stops
+-- OnCollisionEnter is called when a collision with one of the script owner's colliders occurs
+function %%script:OnCollisionEnter(collisionInfo)
+end
+
+-- OnCollisionStay is called when a collision with one of the script owner's colliders is repeated for more than one physics update
+function %%script:OnCollisionStay(collisionInfo)
+end
+
+-- OnCollisionExit is called when a collision with one of the script owner's colliders ends
+function %%script:OnCollisionExit(collisionInfo)
+end
+
+-- OnTriggerEnter is called when a physics object enters one of the script owner's triggers
+function %%script:OnTriggerEnter(triggerInfo)
+end
+
+-- OnTriggerExit is called when a physics object exits one of the script owner's triggers
+function %%script:OnTriggerExit(triggerInfo)
+end
+
+-- OnStop is called when the application stops
 function %%script:OnStop()
 end
 
--- onDestroy is called when the script is removed
+-- OnDestroy is called when the script is removed
 function %%script:OnDestroy()
 end
 
