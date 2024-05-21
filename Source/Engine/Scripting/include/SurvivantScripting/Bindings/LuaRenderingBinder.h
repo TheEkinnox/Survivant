af77bf980c124de47a44454132b13664a49e24fa
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-05-20 22:18:52 -0400

    Add Camera, Color, Projection type & Layers lua bindings

--- Source/Engine/Scripting/include/SurvivantScripting/Bindings/LuaRenderingBinder.h
+++ Source/Engine/Scripting/include/SurvivantScripting/Bindings/LuaRenderingBinder.h
@@ -18,9 +18,33 @@ namespace SvScripting::Bindings
 
     private:
         /**
+         * \brief Creates or updates the color bindings in the given lua state
+         * \param p_luaState The target lua state
+         */
+        static void BindColor(sol::state& p_luaState);
+
+        /**
+         * \brief Creates or updates the layer bindings in the given lua state
+         * \param p_luaState The target lua state
+         */
+        static void BindLayers(sol::state& p_luaState);
+
+        /**
+         * \brief Creates or updates the projection type bindings in the given lua state
+         * \param p_luaState The target lua state
+         */
+        static void BindProjectionTypes(sol::state& p_luaState);
+
+        /**
          * \brief Creates or updates the model component bindings in the given lua state
          * \param p_luaState The target lua state
          */
         static void BindModelComponent(sol::state& p_luaState);
+
+        /**
+         * \brief Creates or updates the camera component bindings in the given lua state
+         * \param p_luaState The target lua state
+         */
+        static void BindCameraComponent(sol::state& p_luaState);
     };
 }
