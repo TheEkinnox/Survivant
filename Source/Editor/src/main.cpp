aa74a06eac1e2d4f8fb2a520aa266e67c6a85bc0
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-05-29 10:00:15 -0400

    Add memory leak check to editor and runtime mains

--- Source/Editor/src/main.cpp
+++ Source/Editor/src/main.cpp
@@ -1,7 +1,16 @@
+#if defined(_DEBUG) && defined(_MSC_VER)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
 #include "SurvivantEditor/Core/EngineApp.h"
 
 int main()
 {
+#if defined(_DEBUG) && defined(_MSC_VER)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
     SvEditor::Core::EngineApp app;
 
     app.Init();
--- Source/Runtime/src/main.cpp
+++ Source/Runtime/src/main.cpp
@@ -1,11 +1,20 @@
+#if defined(_DEBUG) && defined(_MSC_VER)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
 #include "SurvivantRuntime/RuntimeApp.h"
 
 int main()
 {
+#if defined(_DEBUG) && defined(_MSC_VER)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
     SvRuntime::RuntimeApp app;
 
     app.Init();
     app.Run();
 
-	return 0;
+    return 0;
 }
