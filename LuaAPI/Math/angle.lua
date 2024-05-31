d14fb3bb54ea39231b0194b0bc57a538aed31575
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-05-31 05:47:40 -0400

    Add new angle bindings to lua api

--- LuaAPI/Math/angle.lua
+++ LuaAPI/Math/angle.lua
@@ -2,6 +2,8 @@
 
 ---@class Degree
 ---@field public raw number READ-ONLY
+---@field public rawDegree number READ-ONLY
+---@field public rawRadian number READ-ONLY
 Degree = {}
 
 ---Creates an angle in degrees
@@ -16,8 +18,20 @@ function Degree.new(angle) end
 ---@param useNegative boolean
 function Degree:Wrap(useNegative) end
 
+---Gets the angle in degrees wrapped in the range [-180, 180[ if useNegative or to range [0, 360[
+---@param useNegative boolean
+---@return number
+function Degree:GetWrappedDegree(useNegative) end
+
+---Gets the angle in radian wrapped in the range [-pi, pi[ if useNegative or to range [0, 2pi[
+---@param useNegative boolean
+---@return number
+function Degree:GetWrappedRadian(useNegative) end
+
 ---@class Radian
 ---@field public raw number READ-ONLY
+---@field public rawDegree number READ-ONLY
+---@field public rawRadian number READ-ONLY
 Radian = {}
 
 ---Creates an angle in radian
@@ -28,6 +42,16 @@ Radian = {}
 ---@overload fun(degree : Degree) : Radian
 function Radian.new(angle) end
 
----Wraps the angle in the range [-180, 180[ if useNegative or to range [0, 360[
+---Wraps the angle in the range [-pi, pi[ if useNegative or to range [0, 2pi[
 ---@param useNegative boolean
 function Radian:Wrap(useNegative) end
+
+---Gets the angle in degrees wrapped in the range [-180, 180[ if useNegative or to range [0, 360[
+---@param useNegative boolean
+---@return number
+function Radian:GetWrappedDegree(useNegative) end
+
+---Gets the angle in radian wrapped in the range [-pi, pi[ if useNegative or to range [0, 2pi[
+---@param useNegative boolean
+---@return number
+function Radian:GetWrappedRadian(useNegative) end
